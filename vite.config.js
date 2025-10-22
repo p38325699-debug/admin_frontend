40159bb44e7aff050117b933f734e1bfdc2aee59
p38325699-debug <p38325699@gmail.com>
2025-10-23 00:10:20 +0530

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/', // important for correct routing on custom domains
+  build: {
+    outDir: 'dist',
+  },
 })
